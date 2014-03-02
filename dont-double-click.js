f8b409658d4916af1f5c1bdd8b9b517019eec1b2
Andrew Sun <as-com@users.noreply.github.com>
2014-03-02 16:17:58 -0500

    Create dont-double-click.js
    Let's get started!

---
+++ dont-double-click.js
@@ -0,0 +1,8 @@
+(function(){
+	links = document.getElementsByTagName("a");
+	for(i=0; i < links.length; i++) {
+		links[i].addEventListener("dblclick", function(){
+			// Do something (popup window?)
+		};
+	}
+})();
