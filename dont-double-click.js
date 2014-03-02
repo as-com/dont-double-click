1e51dfc57dedeb0e9c4271f8887a7b616d107259
Andrew Sun <as-com@users.noreply.github.com>
2014-03-02 16:38:31 -0500

    Make the event listener more compatible
    Stupid IE...

--- dont-double-click.js
+++ dont-double-click.js
@@ -1,8 +1,24 @@
 (function(){
-	links = document.getElementsByTagName("a");
-	for(i=0; i < links.length; i++) {
-		links[i].addEventListener("dblclick", function(){
+	// Event attachment helper function
+	var addEventSniffer = document.body;
+	var addEvent;
+	if (addEventSniffer.addEventListener) { // Good browsers
+		addEvent = function(event, element, handler) {
+			element.addEventListener(event, handler, false);
+		}
+	} else if (addEventSniffer.attachEvent) { // Bad IE
+		addEvent = function(event, element, handler) {
+			element.attachEvent("on" + event, handler);
+		}
+	} else {
+		addEvent = function(event, element, handler) {
+			element[event] = handler;
+		}
+	}
+	var anchors = document.getElementsByTagName("a");
+	for(i = 0; i < links.length; i++) {
+		addEvent("dblclick", links[i], function(){
 			// Do something (popup window?)
-		};
+		});
 	}
 })();
